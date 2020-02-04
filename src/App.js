28d45c52e81bed390c27bcca22eb287e293095ea
orangejuicetin <justinchoi2000@gmail.com>
2020-02-03 23:20:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-import Nav from './components/Nav'
-import Courses from './components/Courses'
-import Cart from './components/Cart'
+import Nav from './components/Nav';
+import { CourseDisplay } from './components/CourseDisplay';
+import Cart from './components/Cart';
+
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <>
         <Nav />
-        <div style={{
-          width: '100%',
-          boxSizing: 'border-box',
-          padding: '0 calc(1rem + 10%)',
-        }}>
-          <Courses />
+        <div
+          style={{
+            width: '100%',
+            boxSizing: 'border-box',
+            padding: '0 calc(1rem + 10%)'
+          }}
+        >
+          <CourseDisplay />
           <Cart />
         </div>
       </>
@@ -23,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default App;
